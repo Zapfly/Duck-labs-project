6dd0d852ec20c3b29c65b6f3a40bbe6504aee9df
Lucas Seeger <seegerlucas295@gmail.com>
2020-05-24 10:21:31 -0600

    feat: working with jQuery

---
+++ src/scripts/index.js
@@ -0,0 +1,126 @@
+
+function cleanOutElement(id) {
+  $('#'+id).html('')
+}
+
+function appendHtml(id, htmlToAdd) {
+  $('#'+id).append(htmlToAdd)
+}
+
+function setInputValue(id, newValue) {
+    return $('#'+id).val(newValue)
+}
+
+function getInputValue(id) {
+  return $('#'+id).val()
+}
+
+function hide(id) {
+  $('#'+id).hide()
+}
+
+function show(id) {
+  $('#'+id).show()
+}
+
+function disable(id) {
+  $('#'+id).prop("disabled", true)
+}
+
+function enable(id) {
+  $('#'+id).prop("disabled", false)
+}
+
+function inputHasSomeText(id) {
+  return getInputValue(id) !== ''
+}
+
+function todaysDateString() {
+  return new Date().toISOString().substring(0,10)
+}
+
+// getting and setting posts on the page
+examplePost = { postText: "Hi, this is a test post 0", image:"", author: "Tony Enerson", postDate: "2020-05-19"}
+
+function addPostToPage(post) {
+    let postHtml = `<div class="post-card card">${post.postText}<div class="post-footer">${post.author}: ${post.postDate}</div></div>`
+    appendHtml('newsFeed', postHtml)
+}
+
+function updatePagePosts(posts) {
+  cleanOutElement('postsArea')
+  posts.forEach(function(post) {
+    addPostToPage(post)
+  })
+}
+  
+function getPostFromForm() {
+  let authorName 
+  if (getInputValue('authorText') === "") {
+    authorName = 'Anonymous'
+  } else {
+    authorName = getInputValue('authorText')
+  }
+  return {
+    postText: getInputValue('postText'),
+    author: authorName,
+    postDate: todaysDateString()
+  }
+}
+
+function postButtonPressed() {
+  let postToAdd = getPostFromForm()
+  if (inputHasSomeText("postText")) {
+    postPostToServerAndUpdatePage(postToAdd)
+  }
+}
+
+function summonEntryFormButtonPressed() {
+  show('postEntryArea')
+  hide('summonEntryFormButton')
+}
+
+function clearBoardButtonPressed() {
+  cleanOutElement('postsArea')
+}
+
+//---- server interaction
+function postPostToServerAndUpdatePage(post) {
+    $.ajax({
+        url:'/api/v1/addPost',
+        type:"POST",
+        data:JSON.stringify(post),
+        contentType:"application/json; charset=utf-8",
+        success: function() {
+            console.log('In post callback')
+            updatePostsFromServer()
+        },
+        fail: function(error) {
+            // what do we do here?
+        }
+    })
+}
+
+function updatePostsFromServer() {
+    $.getJSON('/api/v1/posts')
+     .done(function(posts) {
+         updatePagePosts(posts)
+     })
+     .fail(function(error) {
+        // what do we do here????
+     })
+}
+
+$(document).ready(function() {
+  updatePostsFromServer()
+  hide('postEntryArea')
+})
+
+// some test data lying around
+testPosts = [
+    { postText: "Hi, this is a test post 0", author: "Tony Enerson", postDate: "2020-05-19"},
+    { postText: "Hi, this is a test post 1", author: "Chris Desmarais", postDate: "2020-05-20"},
+    { postText: "Hi, this is a test post 2", author: "Sheldon Manabat", postDate: "2020-05-20"},
+    { postText: "Hi, this is a test post 3", author: "Tony Enerson", postDate: "2020-05-21"},
+    { postText: "Hi, this is a test post 4", author: "Tony Enerson", postDate: "2020-05-22"}
+]
--- src/scripts/index.test.js
+++ src/scripts/index.test.js
@@ -1,5 +1,33 @@
+// import './jquery-3.5.1'
+import { cleanOutElement, appendHtml } from './index.js';
+// const index = require('./index')
+
 const object = {item1: 1, item2: 2, item3: 3}
 
 test('testing tests', () => {
     expect(object.item1).toEqual(1)
+    console.log(index)
+})
+
+// import appendHtml from './index'
+
+
+test('Remembernig how to Mock', () => {
+    document.body.id = "body"
+    appendHtml(
+        'body',
+        `<div class="post-card card">
+            <div id='postText0' class="post-text">Look at this picture! Don't I look amazing!</div>
+            <div class="post-content"></div>
+            <img src="" class="like-button" /> 
+        </div>`
+    )
+    // document.body.append(
+    //     `<div class="post-card card">` + 
+    //         `<div id='postText0' class="post-text">Look at this picture! Don't I look amazing!</div>` +
+    //         `<div class="post-content"></div>` +
+    //         `<img src="" class="like-button" /> ` +
+    //     `</div>`
+    //     )
+//     expect(document.getElementById('postText0')).toEqual('')
 })
