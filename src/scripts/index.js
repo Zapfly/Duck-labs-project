5956b49b458b6373b83a8f7a9871db116c4d68fa
asilva-cell <achapellin@gmail.com>
2020-05-26 13:17:56 -0600

    fixed $ reference error

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,191 +1,205 @@
 // require("./jquery")
 
 function cleanOutElement(id) {
-  $('#'+id).html('')
+	$("#" + id).html("");
 }
 
 function appendHtml(id, htmlToAdd) {
-  $('#'+id).append(htmlToAdd)
+	$("#" + id).append(htmlToAdd);
 }
 
 function setInputValue(id, newValue) {
-    return $('#'+id).val(newValue)
+	return $("#" + id).val(newValue);
 }
 
 function getInputValue(id) {
-  return $('#'+id).val()
+	return $("#" + id).val();
 }
 
 function hide(id) {
-  $('#'+id).hide()
+	$("#" + id).hide();
 }
 
 function show(id) {
-  $('#'+id).show()
+	$("#" + id).show();
 }
 
 function disable(id) {
-  $('#'+id).prop("disabled", true)
+	$("#" + id).prop("disabled", true);
 }
 
 function enable(id) {
-  $('#'+id).prop("disabled", false)
+	$("#" + id).prop("disabled", false);
 }
 
 function inputHasSomeText(id) {
-  return getInputValue(id) !== ''
+	return getInputValue(id) !== "";
 }
 
 function todaysDateString() {
-  return new Date().toISOString().substring(0,10)
+	return new Date().toISOString().substring(0, 10);
 }
 
 // getting and setting posts on the page
-examplePost = { postText: "Hi, this is a test post 0", image:"", author: "Tony Enerson", postDate: "2020-05-19"}
+examplePost = {
+	postText: "Hi, this is a test post 0",
+	image: "",
+	author: "Tony Enerson",
+	postDate: "2020-05-19"
+};
 
 function addPostToPage(post) {
-  console.log("from addPostToPage")
-  console.log(post)
-    let postHtml = `
+	console.log("from addPostToPage");
+	console.log(post);
+	let postHtml = `
       <div class="post-card card" id="${post.uid}">
         ${post.postText}
         <button onclick="deleteButtonPressed(${post.uid})">&#10005;</button>
         <div class="post-footer">
-          <img src="like-button" onClick="likeButtonPressed()" />
+          <img src="images/like-icon.png" onClick="likeButtonPressed()" />
           ${post.author}: ${post.postDate}
         </div>
       </div>
-    `
-    appendHtml('newsFeed', postHtml)
+    `;
+	appendHtml("newsFeed", postHtml);
 }
 
 function updatePagePosts(posts) {
-  cleanOutElement('newsFeed')
-  posts.forEach(function(post) {
-    addPostToPage(post)
-  })
+	cleanOutElement("newsFeed");
+	posts.forEach(function(post) {
+		addPostToPage(post);
+	});
 }
-  
+
 function getPostFromForm() {
-  let authorName 
-  if (inputHasSomeText("statusInputField")) {
-    authorName = 'Anonymous'
-  } else {
-    authorName = getInputValue('authorText')
-  }
-  return {
-    postText: getInputValue('statusInputField'),
-    author: authorName,
-    postDate: todaysDateString(),
-    uid: String(new Date().getTime())
-  }
+	let authorName;
+	if (inputHasSomeText("statusInputField")) {
+		authorName = "Anonymous";
+	} else {
+		authorName = getInputValue("authorText");
+	}
+	return {
+		postText: getInputValue("statusInputField"),
+		author: authorName,
+		postDate: todaysDateString(),
+		uid: String(new Date().getTime())
+	};
 }
 
 function postButtonPressed() {
-  let postToAdd = getPostFromForm()
-  if (inputHasSomeText("statusInputField")) {
-    postPostsToServerAndUpdatePage(postToAdd)
-  }
+	let postToAdd = getPostFromForm();
+	if (inputHasSomeText("statusInputField")) {
+		postPostsToServerAndUpdatePage(postToAdd);
+	}
 }
 
 function summonEntryFormButtonPressed() {
-  show('postEntryArea')
-  hide('summonEntryFormButton')
+	show("postEntryArea");
+	hide("summonEntryFormButton");
 }
 
 function clearNewsFeedButtonPressed() {
-  clearPostsFromServer()
+	clearPostsFromServer();
 }
 
 function deleteButtonPressed(body) {
-  console.log("deleteButtonPressed")
-  let data = {uid: String(body)}
+	console.log("deleteButtonPressed");
+	let data = { uid: String(body) };
 
-  deleteFromServer(data)  
-
+	deleteFromServer(data);
 }
 
 //---- server interaction
 function postPostsToServerAndUpdatePage(post) {
-    $.ajax({
-        url:'/api/v1/addPost',
-        type:"POST",
-        data:JSON.stringify(post),
-        contentType:"application/json; charset=utf-8",
-        success: function() {
-            console.log('In post callback')
-            updatePostsFromServer()
-        },
-        fail: function(error) {
-            // what do we do here?
-        }
-    })
+	$.ajax({
+		url: "/api/v1/addPost",
+		type: "POST",
+		data: JSON.stringify(post),
+		contentType: "application/json; charset=utf-8",
+		success: function() {
+			console.log("In post callback");
+			updatePostsFromServer();
+		},
+		fail: function(error) {
+			// what do we do here?
+		}
+	});
 }
 
 function updatePostsFromServer() {
-    $.getJSON('/api/v1/posts')
-     .done(function(posts) {
-         updatePagePosts(posts)
-     })
-     .fail(function(error) {
-        // what do we do here????
-     })
+	$.getJSON("/api/v1/posts")
+		.done(function(posts) {
+			updatePagePosts(posts);
+		})
+		.fail(function(error) {
+			// what do we do here????
+		});
 }
 
 function clearPostsFromServer() {
-  $.ajax({
-    url:'/api/v1/clear',
-    type:"POST",
-    success: function() {
-        console.log('server has been cleared')
-        updatePostsFromServer()
-      },
-    fail: function(error) {
-        // what do we do here?
-    }
-})  
+	$.ajax({
+		url: "/api/v1/clear",
+		type: "POST",
+		success: function() {
+			console.log("server has been cleared");
+			updatePostsFromServer();
+		},
+		fail: function(error) {
+			// what do we do here?
+		}
+	});
 }
 
 function deleteFromServer(post) {
-  console.log("from deleteFromServer")
-  console.log(post)
-  $.ajax({
-    url:'/api/v1/delete',
-    type:"POST",
-    data:JSON.stringify(post),
-    contentType:"application/json; charset=utf-8",
-    success: function() {
-        console.log('message has been deleted')
+	console.log("from deleteFromServer");
+	console.log(post);
+	$.ajax({
+		url: "/api/v1/delete",
+		type: "POST",
+		data: JSON.stringify(post),
+		contentType: "application/json; charset=utf-8",
+		success: function() {
+			console.log("message has been deleted");
 
-        updatePostsFromServer()
-      },
-    fail: function(error) {
-        // what do we do here?
-    }
-})  
+			updatePostsFromServer();
+		},
+		fail: function(error) {
+			// what do we do here?
+		}
+	});
 }
 
-
-// jQuery(document).ready(function($){
-
-//   updatePostsFromServer()
-//   hide('postEntryArea')
-  
-//   });
-
-// $(document).ready(function() {
-//   updatePostsFromServer()
-//   hide('postEntryArea')
-// })
-
-
+$(document).ready(function() {
+	updatePostsFromServer();
+	// hide("postEntryArea");
+});
 
 // some test data lying around
 testPosts = [
-    { postText: "Hi, face", author: "Lucas", postDate: "2020-05-19"},
-    { postText: "Hi, this is a test post 0", author: "Tony Enerson", postDate: "2020-05-19"},
-    { postText: "Hi, this is a test post 1", author: "Chris Desmarais", postDate: "2020-05-20"},
-    { postText: "Hi, this is a test post 2", author: "Sheldon Manabat", postDate: "2020-05-20"},
-    { postText: "Hi, this is a test post 3", author: "Tony Enerson", postDate: "2020-05-21"},
-    { postText: "Hi, this is a test post 4", author: "Tony Enerson", postDate: "2020-05-22"}
-]
+	{ postText: "Hi, face", author: "Lucas", postDate: "2020-05-19" },
+	{
+		postText: "Hi, this is a test post 0",
+		author: "Tony Enerson",
+		postDate: "2020-05-19"
+	},
+	{
+		postText: "Hi, this is a test post 1",
+		author: "Chris Desmarais",
+		postDate: "2020-05-20"
+	},
+	{
+		postText: "Hi, this is a test post 2",
+		author: "Sheldon Manabat",
+		postDate: "2020-05-20"
+	},
+	{
+		postText: "Hi, this is a test post 3",
+		author: "Tony Enerson",
+		postDate: "2020-05-21"
+	},
+	{
+		postText: "Hi, this is a test post 4",
+		author: "Tony Enerson",
+		postDate: "2020-05-22"
+	}
+];
